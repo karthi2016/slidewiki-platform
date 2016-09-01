ea1518021727baf21a62f4d22d28e9a095b12e54
Benjamin Wulff <benjamin.wulff.de@ieee.org>
2016-09-01 12:12:52 +0200

    SWAD-15 added support for more than one deployment host to travis stuff

--- travis_scripts/dockerhub.sh
+++ travis_scripts/dockerhub.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+repo='slidewiki/platform'
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t slidewiki/platform ./
-docker push slidewiki/platform
+result="$(docker build -q .)"
+if [[ $result == sha256:* ]]; then
+    image_hash="$(echo $result | sed 's/sha256://')"
+    echo "image hash: ${image_hash}"
+    for tag in "$@"
+    do
+        echo "tagging image: $repo:$tag"
+        docker tag $image_hash $repo:$tag
+        echo "pushing $repo:$tag to DockerHub"
+        docker push $repo:$tag
+    done
+else
+    echo "Docker build failed!"
+    exit -1
+fi
